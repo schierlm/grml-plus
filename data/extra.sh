ab1ef6890404e17b23acf78e5f68266e73dbb4f5
Michael Schierl <schierlm@gmx.de>
2013-10-26 21:57:01 +0200

    Initial version

---
+++ data/extra.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+if [ ! -f /tmp/grml-plus/devicename ]; then
+	echo "run make-grml-plus first!"
+	exit -1
+fi
+. /tmp/grml-plus/devicename
+echo "Mounting $DEVICE to /tmp/grml-plus/mnt."
+mkdir -p /tmp/grml-plus/mnt 
+umount /tmp/grml-plus/mnt 2>/dev/null || true
+mount $DEVICE /tmp/grml-plus/mnt
+cd `dirname $0`/data
+DATADIR=`pwd`
+cd /tmp/grml-plus/mnt
+
+extra_cleanup() {
+	echo "Cleaning up."
+	cd $DATADIR/..
+	umount /tmp/grml-plus/mnt
+	echo "Done. Enjoy your new grml-plus USB drive or add more extras!"
+}
---
+++ data/patch-grub-mbr.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+# GRUB 1.99 by default starts its MBR with EB 63 90, which is used by
+# some crappy embedded devices to mis-detect it as a FAT32 boot record,
+# thereby assuming that the device is not partitioned, but a FAT32
+# filesystem is stored directly on the device. This assumption makes 
+# it impossible to use this USB drive with these devices. Therefore, we
+# patch the MBR to start with the (as far as x86 instructions are
+# concerned) equivalent code 90 EB 62.
+#
+# While we are at it, we will also patch the loading message to read
+# GRUB USB MBR
+# to make it easier to distinguish it from another GRUB installed on the
+# machine we want to use our device on.
+
+open DEV, "+<", $ARGV[0] or die $!;
+binmode DEV;
+
+sub patch {
+	my ($index, $before, $after) = @_;
+	my $actual;
+	read(DEV, $actual, length $before);
+	if ($before eq $actual) {
+		print "Applying patch $index...\n";
+		seek(DEV, - length $before, 1);
+		print DEV $after;
+	} elsif ($after eq $actual) {
+		print "Patch $index already applied.\n";
+	} else {
+		die("Patch $index failed, unexpected bytes detected.");
+	}
+}
+
+patch(1, "\xEB\x63\x90", "\x90\xEB\x62");
+seek(DEV, 792, 1);
+patch(2, "loading", "USB MBR");
+close(DEV);
+
+print "MBR patched successfully\n";
