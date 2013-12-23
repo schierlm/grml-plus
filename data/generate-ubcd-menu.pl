3d432fcb12307316f324f36301779389d88dc32c
Michael Schierl <schierlm@gmx.de>
2013-12-23 22:36:54 +0100

    Complete Ultimate Boot CD support
    Tested with UBCD 5.2.6 and 5.2.8.

--- data/generate-ubcd-menu.pl
+++ data/generate-ubcd-menu.pl
@@ -45,15 +45,25 @@ while(<MAIN>) {
 		last;
 	} elsif (/^CONFIG (\/pmagic\/[^ ]+)( [^ ]+)?$/) {
 		print MENU "submenu \"$label\" {\n";
-		# TODO;
+		print MENU <<ENTRIES;
+	menuentry "Run from RAM" {
+		linux (ubcd)/pmagic/bzImage edd=on vga=normal vmalloc=385MiB iso_filename=\$iso_path
+		initrd (ubcd)/pmagic/initrd.img
+	}
+
+	menuentry "Run from Media" {
+		linux (ubcd)/pmagic/bzImage edd=on vga=normal boot=live eject=no vmalloc=384MiB iso_filename=\$iso_path
+		initrd (ubcd)/pmagic/initrd.img
+	}
+ENTRIES
 		print MENU "}\n";
-	} elsif (/^CONFIG \/([^ ]+)?$/) {
+	} elsif (/^CONFIG \/([^ ]+)$/) {
 		$cfgfile=$1;
 		($base=$cfgfile) =~ s#.*/(.*)\.cfg#$1#;
 
 		open SUB, "<", "$isodir/$cfgfile" or die $!;
 		open SUBDESC, ">", "$confdir/$base.txt" or die $!;
-		
+
 		print MENU <<HEADER;
 submenu "$label" {
 	menuentry "[description]" {
@@ -68,7 +78,6 @@ submenu "$label" {
         	unset pager
 	}
 HEADER
-		
 		while(<SUB>) {
         		last if /^CONFIG \/ubcd\/menus\/syslinux\/main.cfg\r?\n$/;
 		}
@@ -142,13 +151,114 @@ HEADER
                 		print MENU "    linux16 (ubcd)/$kernel\n";
                 		print MENU "}\n";
 			} elsif (/^CONFIG \/ubcd\/boot\/memtest.*$/) {
-				# TODO
-			} elsif (/^CONFIG \/ubcd\/menus\/syslinux\/hdd\/.*$/) {
-				# TODO
+				print MENU "menuentry \"$label\" {\n";
+				print MENU "    linux16 (ubcd)/ubcd/boot/memtest86/memtest\n";
+				print MENU "}\n";
+			} elsif (/^CONFIG \/(ubcd\/menus\/syslinux\/hdd\/.*)$/) {
+				$scfgfile=$1;
+				($sbase=$scfgfile) =~ s#.*/(.*)\.cfg#$1#;
+
+				open SSUB, "<", "$isodir/$scfgfile" or die $!;
+				open SSUBDESC, ">", "$confdir/$sbase.txt" or die $!;
+
+				print MENU <<HEADER;
+submenu "$label" {
+	menuentry "[description]" {
+			set pager=1
+			cat $grubconfdir/$sbase.txt
+			set tmp=\$color_normal
+			set color_normal=\$color_highlight
+			echo -n --END--
+			set color_normal=\$tmp
+			read tmp
+			unset tmp
+			unset pager
+	}
+HEADER
+
+				while(<SSUB>) {
+						last if /^CONFIG \/ubcd\/menus\/syslinux\/hdd.cfg\r?\n$/;
+				}
+
+				while(<SSUB>) {
+					s/ *\r?\n$//;
+					if (/^LABEL -$/ or /^$/) {
+							# ignore
+					} elsif (/^MENU LABEL (.*)$/) {
+							$label=$1;
+					} elsif (/^TEXT HELP$/) {
+							print SSUBDESC "$label:\n\n";
+							while(<SSUB>) {
+									s/\r?\n$//;
+									last if /^ENDTEXT$/;
+									print SSUBDESC $_.$/;
+							}
+							print SSUBDESC $/.$/;
+					} elsif (/^COM32 linux.c32 memdisk$/) {
+								$_=<SSUB>;
+								s/\r?\n$//;
+								die unless /^INITRD ([^ ]+)$/;
+								$initrd=$1;
+								$_=<SSUB>;
+						$_="" unless defined $_;
+								s/\r?\n$//;
+						if (/^$/) {
+							$cmd="";
+						} elsif (/^APPEND (.*)$/) {
+							$cmd=" $1";
+						} else {
+							die "Unexpected option for $label in $sbase.cfg: '$_'\n";
+						}
+								print MENU "menuentry \"$label\" {\n";
+								print MENU "    linux16 (grml)/boot/addons/memdisk$cmd\n";
+								print MENU "    initrd16 (ubcd)$initrd\n";
+								print MENU "}\n";
+					} elsif (/^COM32 linux.c32 \/([^ ]+)$/) {
+						$kernel=$1;
+						$_=<SSUB>;
+						if (defined $_) {
+							s/\r?\n$//;
+							die unless /^INITRD ([^ ]+)$/;
+							$initrd=$1;
+							$_=<SSUB>;
+							s/\r?\n$//;
+							if (/^$/) {
+								$cmd="";
+							} elsif (/^APPEND (.*)$/) {
+								$cmd=" $1";
+							} else {
+								die "Unexpected option for $label in $base.cfg: '$_'\n";
+							}
+						} else {
+							$initrd="";
+							$cmd="";
+						}
+						print MENU "menuentry \"$label\" {\n";
+						print MENU "    linux16 (ubcd)/$kernel$cmd\n";
+						print MENU "    initrd16 (ubcd)$initrd\n" unless $initrd eq "";
+						print MENU "}\n";
+					} elsif (/^CONFIG \/pmagic\/.*$/) {
+						print MENU "menuentry \"$label (via Parted Magic)\" {\n";
+						print MENU "   echo \"Run Parted Magic!\"\n";
+						print MENU "   read tmp\n";
+						print MENU "   unset tmp\n";
+						print MENU "}\n";
+					} elsif (/^LINUX \/(.*)$/) {
+						$kernel=$1;
+						print MENU "menuentry \"$label\" {\n";
+						print MENU "    linux16 (ubcd)/$kernel\n";
+						print MENU "}\n";
+					} else {
+							die "Unexpected line in $sbase.cfg: '$_'\n";
+					}
+				}
+				close(SSUB);
+				close(SSUBDESC);
+				print MENU "}\n";
 			} elsif (/^COM32 hdt.c32$/) {
 				$_=<SUB>;
 				die unless /^APPEND pciids=.*$/;
-				# TODO
+				# COMBOOT not supported by GRUB2!
         		} else {
                 		die "Unexpected line in $base.cfg: '$_'\n";
         		}
