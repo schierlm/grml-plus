609eefccfaaf760370aeefb3361c21c76103b7c9
Michael Schierl <schierlm@gmx.de>
2014-09-25 21:28:07 +0200

    Fix Secure Boot support
    - Debian's GRUB is patched with no_insmod_on_sb.patch, which does not
      really work in combination with our modularized GRUB. Quick&dirty
      unpatch it (by changing the names of the variables in the binary)
    - Shim requires DER-encoded binary certificates, not Base64 ones.
    - Use skipsign.efi to hook LoadImage calls in loaded binaries to allow
      unsigned binaries to be loaded from GRUB.
    
    - Optionally (and by default) switch from Shim to the Linux Foundation's
      PreLoader, as the user experience is nicer.

---
+++ data/patch-grub-secure-boot.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+
+# Debian's grub version has been patched (no-insmod-on-sb.patch) to
+# disallow loading modules when Secure Boot is enabled. This is
+# presumably used in scenarios where the whole chain from GRUB down to
+# the kernel image is signed - as GRUB modules cannot be signed, the
+# easiest way to disable this bypass is to disable GRUB module loading
+# altogether and instead use a monolithic GRUB image. As we don't want
+# to do this and are too lazy to compile our own GRUB, just patch the
+# variable names that are used for the Secure Boot check.
+
+open DEV, "+<", $ARGV[0] or die $!;
+binmode DEV;
+
+sub patch {
+	my ($index, $before, $after) = @_;
+	my $actual;
+	read(DEV, $actual, length $before);
+	if ($before eq $actual) {
+		print "Applying patch $index...\n";
+		seek(DEV, - length $before, 1);
+		print DEV $after;
+	} elsif ($after eq $actual) {
+		print "Patch $index already applied.\n";
+	} else {
+		die("Patch $index failed, unexpected bytes detected.");
+	}
+}
+
+seek(DEV, 0xa5eb, 0);
+patch(1, "SecureBoot", "XecureBoot");
+seek(DEV, 0xa5f6, 0);
+patch(2, "SetupMode", "XetupMode");
+close(DEV);
+
+print "GRUB patched for Secure Boot successfully.\n";
